047eccf994bb5828f568219e5c44401379c559c4
lucas.cruz <lucas.cruz@ceracare.co.uk>
2021-06-11 15:01:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
